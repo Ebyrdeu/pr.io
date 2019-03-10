6dff68354e134748ed5f9dab4265503ade2e7124
Ebyrdeu <42122071+Ebyrdeu@users.noreply.github.com>
2019-03-10 15:58:03 +0100

    /

--- mobile_adaptation/js/main.js
+++ mobile_adaptation/js/main.js
@@ -1,3 +1,4 @@
+// Modal
 var btn = document.querySelector('#btn');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
@@ -10,6 +11,30 @@ btn.addEventListener('click', function() {
 close.addEventListener('click', function() {
     modal.classList.remove('modal_active');
 });
+/*  Slider */
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 2,
+                }
+            }
+        ]
 
-
+    });
+});
 
